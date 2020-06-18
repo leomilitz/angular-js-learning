2dd5118d981d163d4bf85a43a233353342b1badf
Leonardo Militz <39914866+leomilitz@users.noreply.github.com>
2020-06-18 11:58:18 -0300

    Finalizadas funções de deposito e saque

--- deposito-saque/src/app/deposito-saque/deposito-saque.component.ts
+++ deposito-saque/src/app/deposito-saque/deposito-saque.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deposito-saque.component.css']
 })
 export class DepositoSaqueComponent implements OnInit {
-
+  accountFunds = 200;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  deposit() {
+    var depInput = <HTMLInputElement> document.getElementById("currencyInput");
+    var depositValue = parseFloat(depInput.value);
+
+    this.accountFunds += depositValue;
+  }
+  
+  withdraw() {
+      var depInput = <HTMLInputElement> document.getElementById("currencyInput");
+      var withdrawValue = parseFloat(depInput.value);
+
+      if (withdrawValue <= this.accountFunds) {
+        this.accountFunds -= withdrawValue;
+      }
+      else {
+        alert("Not enough funds.");
+      }
+
+  }
 }
